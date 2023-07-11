80204a552074f9cd9b103b23a5be1af0e20d8e77
Krishna <124855697+itshree001@users.noreply.github.com>
2023-07-12 00:34:26 +1000

    Adding Book by API

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import '../App'
+import {API_URL} from '../API'
+import axios from 'axios'
 
 const BookList = () => {
+
+    const[books, setBooks]= useState([]);
+
+
+    useEffect(()=>{
+        axios.get(API_URL).then(res=>{
+            console.log(res.data)
+            setBooks(res.data)
+        })
+        .catch(err=>console.log(err))
+    },[])
+
   return (
-    <div>BookList</div>
+    <div className='book-list'>
+        {books.map((book)=>(
+            <div key={book.id}>
+                <div>
+                    <h2>{book.title}</h2>
+                </div>
+                <div>
+                    <img src={book.image_url} alt="#" />
+                </div>
+                <div>
+                    <button>Add to favorites</button>
+                </div>
+            </div>
+            
+        ))}
+    </div>
   )
 }
 
